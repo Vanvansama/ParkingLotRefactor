37a2e2025f64a7c7bbf20419b320c8d6fe18d912
Kamisama <953181215@qq.com>
2019-07-15 19:52:45 +0800

    [stefan] refactor: refactor the for loop to stream in SmartParkingBoy

--- src/main/java/com/thoughtworks/tdd/SmartParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/SmartParkingBoy.java
@@ -2,6 +2,8 @@ package com.thoughtworks.tdd;
 
 import com.thoughtworks.tdd.Exception.NotEnoughPositionException;
 
+import java.util.Comparator;
+
 
 /**
  * Create with IDEA
@@ -15,22 +17,11 @@ public class SmartParkingBoy extends Boy {
     @Override
     public Ticket parkingCar(Car car) {
         if (car != null) {
-            Ticket ticket = null;
-            int temp = 0, index = -1;
-            for (int i = 0; i < parkingLotList.size(); i++) {
-                ParkingLot parkingLot = parkingLotList.get(i);
-                if ( parkingLot.positionsAvailable() > temp ){
-                    temp = parkingLot.positionsAvailable();
-                    index = i;
-                }
-            }
-            if (temp>0){
-                ParkingLot parkingLot = parkingLotList.get(index);
-                ticket = parkingLot.parkingCar(car);
-            }else{
+            ParkingLot parkingLot = parkingLotList.stream().max(Comparator.comparingInt(ParkingLot::positionsAvailable)).orElse(null);
+            if (parkingLot == null){
                 throw new NotEnoughPositionException();
             }
-            return ticket;
+            return parkingLot.parkingCar(car);
         }
         return null;
     }
